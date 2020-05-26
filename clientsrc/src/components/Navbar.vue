dd39fe4dc619a8aee81c2f3a16195240673e46c6
mikedilulo <mikedilulo2019@gmail.com>
2020-05-26 10:38:34 -0600

    Navbar name change / "Futbol"

--- clientsrc/src/components/Navbar.vue
+++ clientsrc/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <router-link class="navbar-brand" :to="{ name: 'home' }">Fantasy-Football</router-link>
+    <router-link class="navbar-brand" :to="{ name: 'home' }">Fantasy-Futbol</router-link>
     <button
       class="navbar-toggler"
       type="button"
