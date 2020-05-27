b9294d2e9bd54063fe5ba2cc6aee194d70d438d9
mikedilulo <mikedilulo2019@gmail.com>
2020-05-27 11:33:42 -0600

    Add Top Rankings Link to Navbar home page

--- clientsrc/src/components/Navbar.vue
+++ clientsrc/src/components/Navbar.vue
@@ -20,6 +20,9 @@
         <li class="nav-item" :class="{ active: $route.name == 'home' }">
           <router-link :to="{ name: 'home' }" class="nav-link">App Demo</router-link>
         </li>
+        <li class="nav-item" :class="{ active: $route.name == 'home' }">
+          <router-link :to="{ name: 'home' }" class="nav-link">Top Rankings</router-link>
+        </li>
         <li
           class="nav-item"
           v-if="$auth.isAuthenticated"
