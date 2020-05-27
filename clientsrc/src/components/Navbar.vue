bb80cae10918060f8aa983b62fadf22f611ad231
mikedilulo <mikedilulo2019@gmail.com>
2020-05-27 11:35:21 -0600

    Swap Rankings / App Demo links on Home Page

--- clientsrc/src/components/Navbar.vue
+++ clientsrc/src/components/Navbar.vue
@@ -18,10 +18,10 @@
           <router-link :to="{ name: 'home' }" class="nav-link">Home</router-link>
         </li>
         <li class="nav-item" :class="{ active: $route.name == 'home' }">
-          <router-link :to="{ name: 'home' }" class="nav-link">App Demo</router-link>
+          <router-link :to="{ name: 'home' }" class="nav-link">Top Rankings</router-link>
         </li>
         <li class="nav-item" :class="{ active: $route.name == 'home' }">
-          <router-link :to="{ name: 'home' }" class="nav-link">Top Rankings</router-link>
+          <router-link :to="{ name: 'home' }" class="nav-link">App Demo</router-link>
         </li>
         <li class="nav-item" :class="{ active: $route.name == 'home' }">
           <router-link :to="{ name: 'home' }" class="nav-link">Contact Us</router-link>
