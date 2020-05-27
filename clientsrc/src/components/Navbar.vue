3b0ddd1106ae66210ae9966e9dc40b35340d816f
mikedilulo <mikedilulo2019@gmail.com>
2020-05-27 11:36:52 -0600

    Add Player Stats Link to Navbar Home Page

--- clientsrc/src/components/Navbar.vue
+++ clientsrc/src/components/Navbar.vue
@@ -21,6 +21,9 @@
           <router-link :to="{ name: 'home' }" class="nav-link">Top Rankings</router-link>
         </li>
         <li class="nav-item" :class="{ active: $route.name == 'home' }">
+          <router-link :to="{ name: 'home' }" class="nav-link">Player Stats</router-link>
+        </li>
+        <li class="nav-item" :class="{ active: $route.name == 'home' }">
           <router-link :to="{ name: 'home' }" class="nav-link">App Demo</router-link>
         </li>
         <li class="nav-item" :class="{ active: $route.name == 'home' }">
