a449872dee97e6a07f18cf9905a8548757188ca2
mikedilulo <mikedilulo2019@gmail.com>
2020-05-29 17:30:33 -0600

    Margin player news - navbar

--- clientsrc/src/components/Navbar.vue
+++ clientsrc/src/components/Navbar.vue
@@ -29,7 +29,7 @@
         <li class="nav-item ml-3" :class="{ active: $route.name == 'home' }">
           <router-link :to="{ name: 'home' }" class="nav-link">Team News</router-link>
         </li>
-        <li class="nav-item" :class="{ active: $route.name == 'home' }">
+        <li class="nav-item ml-3" :class="{ active: $route.name == 'home' }">
           <router-link :to="{ name: 'home' }" class="nav-link">Player News</router-link>
         </li>
         <li class="nav-item" :class="{ active: $route.name == 'home' }">
