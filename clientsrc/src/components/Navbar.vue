f5f4c548782e7110ec2ba76fee0f1c0546a6f551
mikedilulo <mikedilulo2019@gmail.com>
2020-05-29 17:33:14 -0600

    Comment out - console log - navbar 2

--- clientsrc/src/components/Navbar.vue
+++ clientsrc/src/components/Navbar.vue
@@ -69,7 +69,7 @@ export default {
       this.$store.dispatch("setBearer", this.$auth.bearer);
       this.$store.dispatch("getProfile");
       // console.log("this.$auth.user: ");
-      console.log(this.$auth.user);
+      // console.log(this.$auth.user);
     },
     async logout() {
       await this.$auth.logout({ returnTo: window.location.origin });
