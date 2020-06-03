4592e937ef72fc4c51adff7e8f989b3880474a19
mikedilulo <mikedilulo2019@gmail.com>
2020-06-02 18:15:30 -0600

    Name change to Player News Div & Export Default

--- clientsrc/src/views/PlayerNews.vue
+++ clientsrc/src/views/PlayerNews.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="player-news">
+  <div class="player-storyline">
     <h1>This is the Player News Page</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "player-news-default"
+  name: "player-storyline-default"
 };
 </script>
 
