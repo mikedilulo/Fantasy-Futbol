efeec86ada45381a3e5cb1802edde4ea2c873454
mikedilulo <mikedilulo2019@gmail.com>
2020-06-03 19:06:47 -0600

    Team News Routerlink for Navbar - Navbar.vue

--- clientsrc/src/components/Navbar.vue
+++ clientsrc/src/components/Navbar.vue
@@ -27,7 +27,7 @@
           <router-link :to="{ name: 'home' }" class="nav-link">Injured Reserve(IR)</router-link>
         </li>
         <li class="nav-item ml-4" :class="{ active: $route.name == 'home' }">
-          <router-link :to="{ name: 'home' }" class="nav-link">Team News</router-link>
+          <router-link :to="{ name: 'team-news' }" class="nav-link">Team News</router-link>
         </li>
         <li class="nav-item ml-4" :class="{ active: $route.name == 'home' }">
           <router-link :to="{ name: 'player-news' }" class="nav-link">Player News</router-link>
