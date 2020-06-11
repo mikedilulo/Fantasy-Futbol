31c304107adae4bf9546cbffa4fc19839ea9c069
mikedilulo <mikedilulo2019@gmail.com>
2020-06-10 18:03:14 -0600

    TODO - Format Changes WomenWC Year - View

--- clientsrc/src/views/WomensWorldCup.vue
+++ clientsrc/src/views/WomensWorldCup.vue
@@ -2,6 +2,7 @@
   <div class="womens-world-cup">
     <h1>This is the Women's World Cup Page!</h1>
     <div>
+      //TODO Need to make sure that the years align going across not vertical
       <h5>1991</h5>
       <h5>1995</h5>
     </div>
