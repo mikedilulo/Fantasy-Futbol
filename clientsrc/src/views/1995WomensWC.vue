8a398c708e7e23c3afa33625c16b86bbad02f4a4
mikedilulo <mikedilulo2019@gmail.com>
2020-06-13 08:26:20 -0600

    1995 H1 Womens WC - Route Testing - View

--- clientsrc/src/views/1995WomensWC.vue
+++ clientsrc/src/views/1995WomensWC.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="1995-womens-world-cup"></div>
+  <div class="1995-womens-world-cup">
+    <h1>This is the 1995 Womens World Cup Page</h1>
+  </div>
 </template>
 
 <script>
