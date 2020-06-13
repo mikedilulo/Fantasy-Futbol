eeeaae92a523f8d7e4fe5e547232d1ca90f119b0
mikedilulo <mikedilulo2019@gmail.com>
2020-06-13 08:25:17 -0600

    1991 Womens WC H1 - Route Testing - View

--- clientsrc/src/views/1991WomensWC.vue
+++ clientsrc/src/views/1991WomensWC.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="1991-womens-world-cup"></div>
+  <div class="1991-womens-world-cup">
+    <h1>This is the 1991 Womens World Cup Page</h1>
+  </div>
 </template>
 
 <script>
