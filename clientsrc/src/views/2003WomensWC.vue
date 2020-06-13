762a3fc81c7174f50f1f31902ac9418c96d1ea0a
mikedilulo <mikedilulo2019@gmail.com>
2020-06-13 08:27:30 -0600

    2003 H1 Womens WC - Route Testing - View

--- clientsrc/src/views/2003WomensWC.vue
+++ clientsrc/src/views/2003WomensWC.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="2003-womens-world-cup"></div>
+  <div class="2003-womens-world-cup">
+    <h1>This is the 2003 Womens World Cup Page</h1>
+  </div>
 </template>
 
 <script>
