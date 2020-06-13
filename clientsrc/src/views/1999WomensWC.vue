e32871d2e5063eda83817a8043fdd34dc4a138f6
mikedilulo <mikedilulo2019@gmail.com>
2020-06-13 08:26:49 -0600

    1999 H1 Womens WC - Route Testing - View

--- clientsrc/src/views/1999WomensWC.vue
+++ clientsrc/src/views/1999WomensWC.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="1999-womens-world-cup"></div>
+  <div class="1999-womens-world-cup">
+    <h1>This is the 1999 Womens World Cup Page</h1>
+  </div>
 </template>
 
 <script>
