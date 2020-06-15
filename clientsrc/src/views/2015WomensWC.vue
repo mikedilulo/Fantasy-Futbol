87cfc37774f5a615241768e7b1dd198dd6a6f04f
mikedilulo <mikedilulo2019@gmail.com>
2020-06-15 17:05:30 -0600

    H1 - 2015 WomensWC.vue - View

--- clientsrc/src/views/2015WomensWC.vue
+++ clientsrc/src/views/2015WomensWC.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="2015-womens-world-cup"></div>
+  <div class="2015-womens-world-cup">
+    <h1>This is the 2015 Womens World Cup Page</h1>
+  </div>
 </template>
 
 <script>
