0b4d01b2287d5dae483afaeeb184bf54d0f29097
mikedilulo <mikedilulo2019@gmail.com>
2020-07-15 17:13:38 -0600

    Original layout for Mens World Cup

--- clientsrc/src/views/MensWorldCup.vue
+++ clientsrc/src/views/MensWorldCup.vue
@@ -2,11 +2,7 @@
   <div class="mens-world-cup">
     <h1>This is the Men's World Cup Page</h1>
     <div>
-      <div>
-        <li class="nav-item" :class="{ active: $route.name == 'mens-world-cup' }">
-          <router-link :to="{ name: 'mens-world-cup-1930' }" class="nav-link">Home</router-link>
-        </li>
-      </div>
+      <h5>1930</h5>
       <h5>1934</h5>
       <h5>1938</h5>
       <h5>1950</h5>
