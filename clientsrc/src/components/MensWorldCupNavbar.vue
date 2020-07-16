1cae893a7f8a79b9fae7677da63d40ba31796a75
mikedilulo <mikedilulo2019@gmail.com>
2020-07-16 17:39:51 -0600

    Navbar color change - from white to black

--- clientsrc/src/components/MensWorldCupNavbar.vue
+++ clientsrc/src/components/MensWorldCupNavbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mens-world-cup-navbar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
