c036c95245074c797c36681954360adf4bd75d2a
mikedilulo <mikedilulo2019@gmail.com>
2020-07-20 09:24:08 -0600

    1970 Link Route from Navbar to Mens WC Page - View

--- clientsrc/src/components/MensWorldCupNavbar.vue
+++ clientsrc/src/components/MensWorldCupNavbar.vue
@@ -51,6 +51,12 @@
               <span class="sr-only">(current)</span>
             </a>
           </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              1970
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
