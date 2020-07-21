9592a5bea9a04c85ea83e2c22db660f845aeb2da
mikedilulo <mikedilulo2019@gmail.com>
2020-07-21 17:16:51 -0600

    1978 Add to Mens World Cup Nav - View

--- clientsrc/src/components/MensWorldCupNavbar.vue
+++ clientsrc/src/components/MensWorldCupNavbar.vue
@@ -63,6 +63,12 @@
               <span class="sr-only">(current)</span>
             </a>
           </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              1978
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
