78490391351d1ba4042a7ff97c46e406824eae64
mikedilulo <mikedilulo2019@gmail.com>
2020-07-25 17:10:33 -0600

    2014 Link add to Mens WC - View Navbar

--- clientsrc/src/components/MensWorldCupNavbar.vue
+++ clientsrc/src/components/MensWorldCupNavbar.vue
@@ -117,6 +117,12 @@
               <span class="sr-only">(current)</span>
             </a>
           </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              2014
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
