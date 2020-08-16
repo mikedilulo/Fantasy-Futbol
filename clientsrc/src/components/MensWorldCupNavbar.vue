83aba5d71466e7aad09f64fa87fc3511855d0ec5
mikedilulo <mikedilulo2019@gmail.com>
2020-08-16 13:11:00 -0600

    TODO for Mens WC / Shows up but will be removed

--- clientsrc/src/components/MensWorldCupNavbar.vue
+++ clientsrc/src/components/MensWorldCupNavbar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mens-world-cup-navbar">
+    //TODO Ensure proper spacing between all of the years to center it out on the page
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        //TODO Ensure proper spacing between all of the years to center it out on the page
         <ul class="navbar-nav">
           <li class="nav-item active">
             <a class="nav-link" href="#">
