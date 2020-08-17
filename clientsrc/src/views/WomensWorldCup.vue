c4727eafd739291bac4085abceb57cd6771fb03d
mikedilulo <mikedilulo2019@gmail.com>
2020-08-17 09:37:12 -0600

    Womens WC Navbar in place for View - Main

--- clientsrc/src/views/WomensWorldCup.vue
+++ clientsrc/src/views/WomensWorldCup.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="womens-world-cup">
+    <WomensWorldCupNavbar />
     <h1>This is the Women's World Cup Page!</h1>
     <div></div>
   </div>
@@ -8,8 +9,12 @@
       //TODO Need the router links for these to open up a new view when clicked on
 
 <script>
+import WomensWorldCupNavbar from "../components/WomensWorldCupNavbar";
 export default {
   name: "womens-world-cup",
+  components: {
+    WomensWorldCupNavbar,
+  },
 };
 </script>
 
