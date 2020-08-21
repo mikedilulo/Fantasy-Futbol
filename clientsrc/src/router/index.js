b2eef0e9ec8e5531bdc5d5052e54cb188ee74c61
mikedilulo <mikedilulo2019@gmail.com>
2020-08-21 15:41:06 -0600

    Component for Route Mens WC 1950

--- clientsrc/src/router/index.js
+++ clientsrc/src/router/index.js
@@ -151,7 +151,7 @@ export default new Router({
     {
       path: "/mens-world-cup/1950",
       name: "mens-world-cup-1950",
-      component: MensWorldCup1938,
+      component: MensWorldCup1950,
     },
   ],
 });
