391e0c01173fc6af7d8199e0f1ad7fa724de7da8
mikedilulo <mikedilulo2019@gmail.com>
2020-08-22 11:54:17 -0600

    Component Mens WC !954 Route - Index

--- clientsrc/src/router/index.js
+++ clientsrc/src/router/index.js
@@ -158,7 +158,7 @@ export default new Router({
     {
       path: "/mens-world-cup/1954",
       name: "mens-world-cup-1954",
-      component: MensWorldCup1950,
+      component: MensWorldCup1954,
     },
   ],
 });
