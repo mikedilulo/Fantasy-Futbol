181431e4ccac734dab6358b56fe639ce82a8b29c
mikedilulo <mikedilulo2019@gmail.com>
2020-08-22 11:53:56 -0600

    Name - 1954 - Mens World Cup - Index - Route

--- clientsrc/src/router/index.js
+++ clientsrc/src/router/index.js
@@ -157,7 +157,7 @@ export default new Router({
     },
     {
       path: "/mens-world-cup/1954",
-      name: "mens-world-cup-1950",
+      name: "mens-world-cup-1954",
       component: MensWorldCup1950,
     },
   ],
