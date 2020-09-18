89c49502c83943580ce81e83c2b882383d8ee67c
mikedilulo <mikedilulo2019@gmail.com>
2020-09-18 11:55:10 -0600

    Womens WC Import for 2003 World Cup index route

--- clientsrc/src/router/index.js
+++ clientsrc/src/router/index.js
@@ -54,6 +54,7 @@ import MensWorldCup1978 from "../views/MensWorldCup1978";
 import WomensWorldCup1991 from "../views/WomensWorldCup1991"
 import WomensWorldCup1991 from "../views/WomensWorldCup1991"
 import WomensWorldCup1991 from "../views/WomensWorldCup1991"
+import WomensWorldCup1991 from "../views/WomensWorldCup1991"
 
 import { authGuard } from "@bcwdev/auth0-vue";
 
